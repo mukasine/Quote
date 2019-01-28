3a5fd2bd1cc41e9d208c6b1b32eacf55f9c60963
mukasine marie claire <mclaremukasine@gmail.com>
2019-01-28 15:05:03 +0200

    add paragraph to show description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import {Quote} from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ 
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Quote';
+    goal = 'Watch Finding Nemo';
+    goals = [
+              new Quote(1,'Watch Finding Nemo',''),
+              new Quote(2,'Buy Cookies',''),
+              new Quote(3, 'Get new Phone Case',''),
+              new Quote(4, 'Get Dog Food',''),
+              new Quote(5, 'Solve math homework',''),
+              new Quote(6, 'Plot my world domination plan',''),
+      
+          ]
+    constructor(){
 }
+  }
+
+
+
+  
