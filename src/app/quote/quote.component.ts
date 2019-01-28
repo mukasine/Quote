162ab137130bc6630f9650a18b665674f8891129
mukasine marie claire <mclaremukasine@gmail.com>
2019-01-28 15:55:08 +0200

    add output property binding

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,7 +7,7 @@ import {Quote} from '../quote'
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-    Quotes = [
+    quotes = [
         new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
         new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
         new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
@@ -15,11 +15,17 @@ export class QuoteComponent implements OnInit {
         new Quote(5, 'Solve math homework','Damn Math'),
         new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
     ]
+toogleDetails(index){
+      this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
   constructor() { }
   ngOnInit() {
   }
 
 }
+
+
+
 
 
 
