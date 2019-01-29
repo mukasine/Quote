af53901182cb9e1b697645126f9021b26a27ea1b
mukasine marie claire <mclaremukasine@gmail.com>
2019-01-29 16:03:29 +0200

    app.component.css

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,5 @@
 export class Quote {
-    
+    public showDescription:boolean
     constructor(public id:number, public name:string,public description:string,public completeDate:Date){
         this.showDescription=false
 
