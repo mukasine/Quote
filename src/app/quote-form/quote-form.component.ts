8968b70942fe68e5cf73a625b1edf99a730513f8
mukasine marie claire <mclaremukasine@gmail.com>
2019-01-29 16:11:25 +0200

    add component

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { , OnInit,Output,EventEmitter } from '@angular/core';
 import {Quote} from '../quote';
 @Component({
   selector: 'app-quote-form',
